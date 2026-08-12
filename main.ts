1dfecd3997ae5b8854c88a2619af696644f90a48
Miguggy <00001200060982sp@al.educacao.sp.gov.br>
2026-08-12 10:42:17 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+let listas: string[] = []
+let letra: string[] = []
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (listas._pickRandom()))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (letra._pickRandom()))
+})
 basic.forever(function () {
-	
+    listas = [
+    "letra",
+    "listas",
+    "c",
+    ""
+    ]
+    letra = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o"
+    ]
 })
